5418b6a3c9413ac42a32c624615bd0eb0d3b222e
gadputut <gadput@gadput.com>
2020-10-25 14:14:25 +0800

    init gadput project

---
+++ src/main/java/com/gadput/User.java
@@ -0,0 +1,8 @@
+package com.gadput;
+
+/**
+ * @Created by Meng Kai
+ * @Date 2020/10/25
+ */
+public class User {
+}
---
+++ src/main/java/com/gadput/test/UserTest.java
@@ -0,0 +1,9 @@
+package com.gadput.test;
+
+/**
+ * @Created by Meng Kai
+ * @Date 2020/10/25
+ */
+public class UserTest {
+
+}
